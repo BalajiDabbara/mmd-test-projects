89e6f3096a0107034ce9dc868601d299c0020b6e
Balaji Dabbara <bdabbara@microsoft.com>
2021-10-17 11:04:06 -0700

    Adding class diagrams

--- OnlineCalculator/OnlineCalculatorApp/Telemetry/OnlineCalculatorLoggerProvider.cs
+++ OnlineCalculator/OnlineCalculatorApp/Telemetry/OnlineCalculatorLoggerProvider.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace online_calculator_app.Telemetry
+namespace OnlineCalculatorApp
 {
     /// <summary>
     /// The OnlineCalculatorLoggerProvider
