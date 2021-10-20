090ae5d2b25cd5960571a3734b057242cb0169a7
Balaji Dabbara <bdabbara@microsoft.com>
2021-10-20 13:04:14 -0700

    Improving expression validator.

--- OnlineCalculator/OnlineCalculatorApp/ExpressionEvaluator/ExpressionProcessor.cs
+++ OnlineCalculator/OnlineCalculatorApp/ExpressionEvaluator/ExpressionProcessor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace OnlineCalculatorApp
 {
@@ -11,32 +12,53 @@ namespace OnlineCalculatorApp
     public class ExpressionProcessor : IExpressionProcessor
     {
         /// <summary>
+        /// Is valid numbers 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private bool IsInputStringHasInValidChars(string input)
+        {
+            string alloweCharRegEx = Environment.GetEnvironmentVariable("ALLOWED_INPUT_CHARACTERS");
+
+            // For unit tests purpose, keeping regular expression ([^[0-9-/*+R()]]*) in constants.
+            alloweCharRegEx = string.IsNullOrEmpty(alloweCharRegEx) ? Constants.ALLOWED_INPUTCHARS : alloweCharRegEx;
+            return Regex.IsMatch(input, alloweCharRegEx);
+        }
+        /// <summary>
         /// Validate whether the input expression has balanced paranthesis.
         /// </summary>
         /// <param name="inputString">The input infix string</param>
         /// <returns>Returns true if expression is balanced otherwise false.</returns>
         private bool AreParanthesisBalanced(string inputString)
         {
-            int openCount = 0;
-            int closedCount = 0;
+            Stack<char> openParenthesesStack = new Stack<char>();
+            bool isExpressionBalanced = true;
+
+            if (IsInputStringHasInValidChars(inputString))
+                return false;
 
             for (int i = 0; i < inputString.Length; i++)
             {
-                if (inputString[0] == Constants.CLOSE_PARANTHESIS)
+                switch (inputString[i])
+                {
+                    case Constants.OPEN_PARANTHESIS:
+                        openParenthesesStack.Push(inputString[i]);
+                        break;
+                    case Constants.CLOSE_PARANTHESIS:
+                        {
+                            if (openParenthesesStack.Count == 0)
+                                isExpressionBalanced = false;
+                            else
+                                openParenthesesStack.Pop();
+                            break;
+                        }
+                }
+
+                if (!isExpressionBalanced)
                     return false;
-
-                if(CalculatorHelper.IsOpeningParenthesis(inputString[i]))
-                {
-                    openCount++;
-                }
-                else if (CalculatorHelper.IsClosingParenthesis(inputString[i]))
-                {
-                    closedCount++;
-                }
-
             }
 
-            return (openCount == closedCount)  ? true : false;
+            return isExpressionBalanced;
         }
 
         /// <summary>
@@ -60,16 +82,7 @@ namespace OnlineCalculatorApp
         /// <returns>Returns true if expression is valid otherwise false.</returns>
         public bool ValidateExpression(string inputString)
         {
-
-            if (string.IsNullOrEmpty(inputString) || 
-                !AreParanthesisBalanced(inputString))
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
+            return string.IsNullOrEmpty(inputString) || !AreParanthesisBalanced(inputString) ? false : true;
         }
     }
 }
